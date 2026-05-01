45149c7fffd4a1d4de5457666e52b9834a7de0c8
Tayyab Atiq <135827062+Tayyab2344@users.noreply.github.com>
2026-05-01 18:24:52 +0500

    images added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -92,7 +92,7 @@ export default function Navbar() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.3 }}
-            className="fixed inset-0 z-[55] md:hidden"
+            className="fixed inset-0 z-[70] md:hidden"
           >
             {/* Backdrop */}
             <div
